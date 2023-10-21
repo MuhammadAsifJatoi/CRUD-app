830020179a069c753370bda0d1f1486fadd679c7
MuhammadAsifJatoi <m.asifjatoi7770@gmail.com>
2023-10-21 23:31:02 +0500

    workdone

--- PNLS.Edusystem/Migrations/20231021170256_actualdone.Designer.cs
+++ PNLS.Edusystem/Migrations/20231021170256_actualdone.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PNLS.Edusystem.Models;
 
@@ -11,9 +12,10 @@ using PNLS.Edusystem.Models;
 namespace PNLS.Edusystem.Migrations
 {
     [DbContext(typeof(AdminDbcontext))]
-    partial class AdminDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20231021170256_actualdone")]
+    partial class actualdone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PNLS.Edusystem/Migrations/20231021170256_actualdone.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PNLS.Edusystem.Migrations
+{
+    public partial class actualdone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "doctertable");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "doctertable",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_doctertable", x => x.id);
+                });
+        }
+    }
+}
